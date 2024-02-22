38b61c5f2c799189d29db1141465590d1a3004a0
Neto test <alevochkina96@gmail.com>
2024-02-22 12:59:32 +0700

    Третье задание полностью готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,22 @@
 function compareArrays(arr1, arr2) {
-  
+	if (arr1.length !== arr2.length) {
+		return false;
+	}
+
+	return arr1.every(function(element, index) {
+		return element === arr2[index];
+	})
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+	if (users.length === 0) {
+		return 0;
+	}
+	const user = users.filter(user => user.gender === gender);
+	if (user.length === 0) {
+		return 0;
+	}
+	const ages = user.map(user => user.age);
+	const averagAge = ages.reduce((sum, age) => sum + age, 0) / ages.length;
+	return averagAge;
 }
